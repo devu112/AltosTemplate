8138b5082559effddd214bc7ce460ed4c4562b1e
devu112 <devikathrayambakam112@gmail.com>
2023-10-16 22:11:24 +0530

    commit 3

--- altosapp/urls.py
+++ altosapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('adminlog', views.adminlog, name='adminlog'),
-    path('adminhome', views.adminhome, name='adminhome'),
+    path('aha', views.aha, name='aha'),
+    # path('adminlog', views.adminlog, name='adminlog'),
+    # path('adminhome', views.adminhome, name='adminhome'),
 ]
--- altosapp/views.py
+++ altosapp/views.py
@@ -11,24 +11,29 @@ def home(request):
     
     return render(request, 'home.html')
 
-def adminhome(request):
-    if request.user.is_authenticated and request.user.is_staff:
-        return render(request, 'adminhome.html')
+def aha(request):
+    
+    return render(request, 'aha.html')
 
-def adminlog(request):
-    if request.method == "POST":
-        username = request.POST['name']
-        password = request.POST['psw']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_staff:
-                login(request, user)
-                return redirect('adminhome')
-            else:
-                login(request, user)
-                messages.info(request, f'Welcome {username}')
-                return redirect('userhome')
-        else:
-            messages.error(request, "Invalid username or password")
-            return redirect('/')
-    return render(request, 'home.html')
+# def adminhome(request):
+#     if request.user.is_authenticated and request.user.is_staff:
+#         return render(request, 'adminhome.html')
+
+# def adminlog(request):
+#     if request.method == "POST":
+#         username = request.POST['name']
+#         password = request.POST['psw']
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             if user.is_staff:
+#                 login(request, user)
+#                 return redirect('adminhome')
+#             else:
+#                 login(request, user)
+#                 messages.info(request, f'Welcome {username}')
+#                 return redirect('userhome')
+#         else:
+#             messages.error(request, "Invalid username or password")
+#             return redirect('/')
+#     return render(request, 'home.html')
+
