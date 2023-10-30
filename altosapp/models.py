9fd72225cf775ca0d042b3486778e86a547c2377
devu112 <devikathrayambakam112@gmail.com>
2023-10-30 19:56:46 +0530

    commit 9

--- altosapp/models.py
+++ altosapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     selected_theme = models.CharField(max_length=100, default='default')
